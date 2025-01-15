5e55ee72a7a7d9b2b09f2f64865d32fefcfa78a1
Levi <70198955+Draysongz@users.noreply.github.com>
2025-01-15 15:45:27 +0100

    sone

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,27 @@
 "use client";
 
 import { Box } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
+import { useUser } from "@/context/context";
+import { useSearchParams } from "next/navigation";
 import Loading from "@/components/Loading";
 import Welcome from "@/components/Welcome";
 
 export const dynamic = "force-dynamic";
 
 function HomeContent() {
-
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-
-  
   const [error, setError] = useState<string | null>(null);
-  const { setUser, user } = useUser();
+  console.log(error)
+  const { setUser } = useUser();
   const searchParams = useSearchParams();
   const referralCode = searchParams.get("referralCode");
 
   const telegramInitData =
     "query_id=AAElBO5_AAAAACUE7n8MOa_y&user=%7B%22id%22%3A2146305061%2C%22first_name%22%3A%22Crypto%22%2C%22last_name%22%3A%22Dray%22%2C%22username%22%3A%22Habibilord%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1729828440&hash=a21590e0fe10c68048781c3b3e22e8ecde0a8b8b163bf4c7a58e5c48855e584e";
 
+  // Handle authentication
   useEffect(() => {
-    // WebApp.expand()
-    // const telegramInitData = WebApp.initData
     if (telegramInitData) {
       fetch("/api/auth", {
         method: "POST",
@@ -48,7 +42,6 @@ function HomeContent() {
         .then((data) => {
           if (data.error) {
             setError(data.error);
-            console.log(error);
           } else {
             console.log("User data:", data);
             setUser(data.user);
@@ -61,8 +54,13 @@ function HomeContent() {
     } else {
       setError("No user data available");
     }
-  }, [referralCode]);
+  }, [referralCode, setUser]);
 
+  // Handle loading state
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, []);
@@ -80,6 +78,8 @@ function HomeContent() {
 
 export default function App() {
   return (
-    <HomeContent />
+    <Suspense>
+      <HomeContent />;
+    </Suspense>
   );
 }
